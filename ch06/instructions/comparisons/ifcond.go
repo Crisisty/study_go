8898c72149a8a918ebce53934e1a2e7a05bd282a
Crisisty <1154917679@qq.com>
2022-07-27 09:46:49 +0800

    init

---
+++ ch06/instructions/comparisons/ifcond.go
@@ -0,0 +1,60 @@
+package comparisons
+
+import (
+	"jvmgo/ch06/instructions/base"
+	"jvmgo/ch06/rtda"
+)
+
+type IFEQ struct{ base.BatchInstruction }
+
+func (self *IFEQ) Execute(frame *rtda.Frame) {
+	val := frame.OperandStack().PopInt()
+	if val == 0 {
+		base.Branch(frame, self.Offset)
+	}
+}
+
+type IFNE struct{ base.BatchInstruction }
+
+func (self *IFNE) Execute(frame *rtda.Frame) {
+	val := frame.OperandStack().PopInt()
+	if val != 0 {
+		base.Branch(frame, self.Offset)
+	}
+}
+
+type IFLT struct{ base.BatchInstruction }
+
+func (self *IFLT) Execute(frame *rtda.Frame) {
+	val := frame.OperandStack().PopInt()
+	if val < 0 {
+		base.Branch(frame, self.Offset)
+	}
+}
+
+type IFLE struct{ base.BatchInstruction }
+
+func (self *IFLE) Execute(frame *rtda.Frame) {
+	val := frame.OperandStack().PopInt()
+	if val <= 0 {
+		base.Branch(frame, self.Offset)
+	}
+}
+
+type IFGT struct{ base.BatchInstruction }
+
+func (self *IFGT) Execute(frame *rtda.Frame) {
+	val := frame.OperandStack().PopInt()
+	if val > 0 {
+		base.Branch(frame, self.Offset)
+	}
+}
+
+type IFGE struct{ base.BatchInstruction }
+
+func (self *IFGE) Execute(frame *rtda.Frame) {
+	val := frame.OperandStack().PopInt()
+	if val >= 0 {
+		base.Branch(frame, self.Offset)
+	}
+}
